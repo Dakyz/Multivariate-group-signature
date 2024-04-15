4da3e8160d512d3dc8407cc55cfd675e39edc57a
Данил <kyznezov2222@gmail.com>
2024-04-16 00:18:54 +0300

    [refactoring] [codedrop] Add simple db interaction singleton class Repository, add some comments to Window class

--- src/constants.py
+++ src/constants.py
@@ -4,5 +4,8 @@ class Constants:
     PATH_TO_WORKER_RES = PATH_TO_RES + "company/user.png"
     PATH_TO_MANAGER_RES = PATH_TO_RES + "company/manager.png"
 
+    # db
+    DB_NAME = "database.db"
+
     # general constants
     COMPANIES_CNT = 2
